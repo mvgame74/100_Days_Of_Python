5dab4d0aa05211dafc66be3772f1440f35e03f90
Maria Valero <mvalerogonzalez@gmail.com>
2021-10-16 20:02:54 +0000

    Day 5 exercise 3

---
+++ Day-5-3.-Exercise-Added-Even-numbers.py
@@ -0,0 +1,11 @@
+#Write your code below this row 👇
+even_numbers_added = 0
+for number in range (1, 101):
+  if number % 2 == 0:
+    even_numbers_added += number
+
+print(even_numbers_added)
+
+#for number in range (1, 101, 2):
+#  even_numbers_added += number
+#
