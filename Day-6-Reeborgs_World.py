469afe2bbc731e09beef87bc8b2ee60446ad2141
Maria Valero <mvalerogonzalez@gmail.com>
2021-11-08 20:15:55 +0000

    Final day 6 with edge cases

--- Day-6-Reeborgs_World.py
+++ Day-6-Reeborgs_World.py
@@ -148,3 +148,23 @@ while not at_goal():
         move()
     else:
         turn_left()
+
+*******************************************************************
+
+MAZE WITH EDGE CASES
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear() and front_is_clear():
+        move()
+    elif right_is_clear():
+        turn_right()
+        move()
+    elif wall_on_right() and front_is_clear():
+        move()
+    else:
+        turn_left()
